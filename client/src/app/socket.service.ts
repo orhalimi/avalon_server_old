bfb6e1b67917bc2e7ea3dfcb09deb0330312d5d0
orhalimi <or50002001@gmail.com>
2022-08-12 18:27:43 +0300

    Cilent on nginx

--- client/src/app/socket.service.ts
+++ client/src/app/socket.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, EventEmitter } from '@angular/core';
-import {AuthService} from './auth.service';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class SocketService {
@@ -17,16 +17,16 @@ export class SocketService {
 
 
   openSocket() {
-    this.socket = new WebSocket('ws://3.121.237.188:12345/ws?token=' + this.authService.token); //localhost
+    this.socket = new WebSocket('ws://127.0.0.1:8080/ws?token=' + this.authService.token); //localhost
     // this.socket = new WebSocket('ws://3.121.195.232:12345/ws?token=' + this.authService.token); //localhost
     this.socket.onopen = event => {
-      this.listener.emit({type: 'open', data: event});
+      this.listener.emit({ type: 'open', data: event });
     }
     this.socket.onclose = event => {
-      this.listener.emit({type: 'close', data: event});
+      this.listener.emit({ type: 'close', data: event });
     }
     this.socket.onmessage = event => {
-      this.listener.emit({type: 'message', data: JSON.parse(event.data)});
+      this.listener.emit({ type: 'message', data: JSON.parse(event.data) });
     };
   }
 
